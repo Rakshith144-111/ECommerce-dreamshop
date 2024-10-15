7aba731bac0703cb536fea71acbc8fa6e8f11362
Rakshith144-111 <rakshithpdrak@gmail.com>
2024-10-15 23:54:29 +0530

    CartRepoChanges

--- src/main/java/com/application/Ecommerce/repository/CartItemRepository.java
+++ src/main/java/com/application/Ecommerce/repository/CartItemRepository.java
@@ -5,5 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.application.Ecommerce.model.CartItem;
 
 public interface CartItemRepository extends JpaRepository<CartItem, Long> {
+	//delete by using the CartID
     void deleteAllByCartId(Long id);
 }
